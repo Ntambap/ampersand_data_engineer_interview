524fe57954c8ad8ac4f3319528d367a067b483b4
Ntambap <37585960+Ntambap@users.noreply.github.com>
2022-07-24 14:36:40 +0300

    Add files via upload

---
+++ people_schema.sql
@@ -0,0 +1,18 @@
+drop table if exists peoples;
+
+create table peoples
+(  
+    given_name   varchar,
+    family_name  varchar,
+    date_of_birth varchar,
+    place_of_birth varchar
+);
+
+drop table if exists places;
+
+create table places
+(
+    city   varchar,
+    county  varchar,
+    country varchar 
+);
